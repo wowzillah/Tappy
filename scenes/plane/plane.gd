1b92a6ed11b234e110582290faebc42e7d222cbf
wowzillah <155591529+wowzillah@users.noreply.github.com>
2024-03-13 11:44:37 +0100

    set up plane collision

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const GRAVITY : float = 700.0
+@onready var sprite : AnimatedSprite2D = $Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +9,16 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	
+	move_and_slide()
+# check if the sprite is touching the barrier(floor); if so, we come to a die state	
+	if is_on_floor() == true:
+		die()
+		
+# the function stop the animation and the physic process
+func die() -> void:
+	sprite.stop()
+	set_physics_process(false)
